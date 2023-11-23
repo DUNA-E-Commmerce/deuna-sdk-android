7b3ac7ae73b77443e0fa0adf511fbe0b20eb174b
silvaignacio <silvasilvaignacio@yahoo.com>
2023-11-23 11:37:04 -0300

    feature: move userToken from config to initElements

--- src/main/java/com/deuna/maven/shared/ApiGatewayUrl.kt
+++ src/main/java/com/deuna/maven/shared/ApiGatewayUrl.kt
@@ -1,5 +1,9 @@
 package com.deuna.maven.shared
 
+/**
+ * The ApiGatewayUrl enum class is used to set the API Gateway URL based on the environment.
+
+ */
 enum class ApiGatewayUrl(val url: String)  {
     DEVELOPMENT("https://api.dev.deuna.io"),
     STAGING("https://api.stg.deuna.io"),
