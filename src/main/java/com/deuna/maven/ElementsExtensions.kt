d30ba0ce6ff3cfbe2290e4c9730f2ce74cd5e555
Darwin Morocho - Deuna <dmorocho@deuna.com>
2024-03-05 12:54:36 -0500

    feat: removed unused imports

--- src/main/java/com/deuna/maven/ElementsExtensions.kt
+++ src/main/java/com/deuna/maven/ElementsExtensions.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import androidx.core.content.ContextCompat
-import com.deuna.maven.checkout.DeunaActivity
 import com.deuna.maven.checkout.domain.ElementType
 import com.deuna.maven.element.DeunaElementActivity
 import com.deuna.maven.element.domain.ElementsCallbacks
