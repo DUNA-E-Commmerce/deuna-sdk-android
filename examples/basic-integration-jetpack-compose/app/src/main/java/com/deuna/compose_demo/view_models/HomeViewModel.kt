58d4a2e58cd5b85aa962b4b9743bfec56b40e300
darwinmorocho-deuna <dmorocho@deuna.com>
2024-03-12 09:37:58 -0500

    added compose example

--- examples/basic-integration-jetpack-compose/app/src/main/java/com/deuna/compose_demo/view_models/HomeViewModel.kt
+++ examples/basic-integration-jetpack-compose/app/src/main/java/com/deuna/compose_demo/view_models/HomeViewModel.kt
@@ -11,62 +11,167 @@ import com.deuna.maven.checkout.domain.*
 import com.deuna.maven.element.domain.*
 import kotlinx.coroutines.*
 
+
+/**
+ * ViewModel for the Home screen, responsible for handling user interactions and data manipulation.
+ * @param deunaSDK The DeunaSDK instance used for payment and card saving operations.
+ */
 class HomeViewModel(private val deunaSDK: DeunaSDK) : ViewModel() {
+
+  // State variables for order token and user token
   val orderToken = mutableStateOf("")
   val userToken = mutableStateOf("")
 
 
+  /**
+   * Initiates the payment process.
+   * @param context The activity context.
+   * @param completion Callback to be invoked upon completion of the payment process.
+   */
   fun payment(
     context: Context,
-    completion: (CheckoutResponse?, DeunaErrorMessage?) -> Unit,
+    completion: (CheckoutResult) -> Unit,
   ) {
     deunaSDK.initCheckout(
       context = context,
       orderToken = orderToken.value.trim(),
-      callbacks = CheckoutCallbacks().apply {
-        onSuccess = {
-          deunaSDK.closeCheckout(context = context)
-          viewModelScope.launch {
-            completion(it, null)
-          }
-        }
-        onError = {
-          deunaSDK.closeCheckout(context = context)
-          viewModelScope.launch {
-            completion(null, it)
-          }
-        }
-        eventListener = { event, _ ->
-          Log.d("DeunaSDK", "on event ${event.value}")
-        }
-      }
+      callbacks = checkoutCallbacks(context, completion)
     )
   }
 
+
+  private fun checkoutCallbacks(
+    context: Context,
+    completion: (CheckoutResult) -> Unit,
+  ): CheckoutCallbacks {
+    return CheckoutCallbacks().apply {
+      onSuccess = { response ->
+        deunaSDK.closeCheckout(context)
+        viewModelScope.launch {
+          completion(CheckoutResult.Success(response))
+        }
+      }
+      onError = { error ->
+        deunaSDK.closeCheckout(context)
+        viewModelScope.launch {
+          completion(CheckoutResult.Error(error))
+        }
+      }
+      eventListener = { event, _ ->
+        when (event) {
+          CheckoutEvent.linkClose -> {
+            viewModelScope.launch {
+              completion(CheckoutResult.Canceled)
+            }
+          }
+
+          CheckoutEvent.changeCart, CheckoutEvent.changeAddress -> {
+            deunaSDK.closeCheckout(context)
+            viewModelScope.launch {
+              completion(CheckoutResult.Canceled)
+            }
+          }
+
+          else -> Log.d("DeunaSDK", "on event ${event.value}")
+        }
+      }
+    }
+  }
+
+
+  /**
+   * Initiates the process of saving the card information.
+   * @param context The activity context.
+   * @param completion Callback to be invoked upon completion of the card saving process.
+   */
   fun saveCard(
     context: Context,
-    completion: (ElementsResponse?, ElementsErrorMessage?) -> Unit,
+    completion: (ElementsResult) -> Unit,
   ) {
     deunaSDK.initElements(
       context = context,
       userToken = userToken.value.trim(),
-      callbacks = ElementsCallbacks().apply {
-        onSuccess = {
-          deunaSDK.closeElements(context = context)
-          viewModelScope.launch {
-            completion(it, null)
-          }
+      callbacks = elementsCallbacks(context, completion)
+    )
+  }
+
+
+  private fun elementsCallbacks(
+    context: Context,
+    completion: (ElementsResult) -> Unit,
+  ): ElementsCallbacks {
+    return ElementsCallbacks().apply {
+      onSuccess = { response ->
+        deunaSDK.closeElements(context)
+        viewModelScope.launch {
+          completion(ElementsResult.Success(response))
         }
-        onError = {
-          deunaSDK.closeElements(context = context)
-          viewModelScope.launch {
-            completion(null, it)
-          }
+      }
+      onError = { error ->
+        deunaSDK.closeElements(context)
+        viewModelScope.launch {
+          completion(ElementsResult.Error(error))
         }
-        eventListener = { event, _ ->
+      }
+      eventListener = { event, _ ->
+        if (event == ElementsEvent.vaultClosed) {
+          viewModelScope.launch {
+            completion(ElementsResult.Canceled)
+          }
+        } else {
           Log.d("DeunaSDK", "on event ${event.value}")
         }
       }
-    )
+    }
   }
+}
+
+
+/**
+ * Sealed classes for representing the results of checkout and element saving processes.
+ */
+
+sealed class CheckoutResult {
+
+  /**
+   * Indicates successful completion of the checkout process.
+   *
+   * @property response The CheckoutResponse object containing details about the completed checkout.
+   */
+  data class Success(val response: CheckoutResponse) : CheckoutResult()
+
+  /**
+   * Indicates an error occurred during the checkout process.
+   *
+   * @property error The DeunaErrorMessage object detailing the error encountered.
+   */
+  data class Error(val error: DeunaErrorMessage) : CheckoutResult()
+
+  /**
+   * Indicates the checkout process was cancelled by the user.
+   */
+  data object Canceled : CheckoutResult()
+}
+
+
+sealed class ElementsResult {
+
+  /**
+   * Indicates successful completion of the element saving process (e.g., saving card information).
+   *
+   * @property response The ElementsResponse object containing details about the saved elements.
+   */
+  data class Success(val response: ElementsResponse) : ElementsResult()
+
+  /**
+   * Indicates an error occurred during the element saving process.
+   *
+   * @property error The ElementsErrorMessage object detailing the error encountered.
+   */
+  data class Error(val error: ElementsErrorMessage) : ElementsResult()
+
+  /**
+   * Indicates the element saving process was cancelled by the user.
+   */
+  data object Canceled : ElementsResult()
 }
